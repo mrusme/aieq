bf1b1f513957a6b65ae6272c6cb9b73a659ef510
Marius <marius@twostairs.com>
2019-04-20 00:32:51 +0200

    Example config.h

---
+++ config.example.h
@@ -0,0 +1,12 @@
+#define WIFI_SSID ""
+#define WIFI_PASS ""
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+#define NTP_SERVER "pool.ntp.org"
+#define NTP_OFFSET 0
+#define NTP_INTERVAL 3600000
+
+#define MEASURE_INTERVAL_MS 300000
+
+byte INFLUXDB_IP[] = {10, 10, 10, 10};
+int INFLUXDB_UDP_PORT = 8089;
